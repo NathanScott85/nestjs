8047b9267287c360791a4ce60fabbf9b1cb6dfb5
NathanScott85 <contact@nathanscott.net>
2020-04-17 22:50:44 +0100

    modified app controller, module, service and main.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // next js infers the type if you do not pass the type as >> : string to the function.
+  // you could set your own decorator by importing the @Header from @nextjs/common.
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
