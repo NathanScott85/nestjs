d0a2ad7e1c82b030a177c5fa4c7a88819dbe0f37
NathanScott85 <contact@nathanscott.net>
2020-04-17 19:26:36 +0100

    modified main.ts, added swagger, updated readme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder().setTitle('Hello world')
+    .setDescription('A Basic Hello World')
+    .setVersion('0.1')
+    .addTag('Hello')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
+  await app.listen( process.env.port || 4000);
 }
 bootstrap();
